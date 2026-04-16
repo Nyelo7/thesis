b1d5a7f1c9c8bbe45021816c3098d2aaf47da71f
Nyelo7 <nyelonyl2@gmail.com>
2026-04-16 10:14:25 +0800

    fixed errors

--- admin/mapi/mapi.php
+++ admin/mapi/mapi.php
@@ -38,7 +38,7 @@ $conn->close();
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Cemetery Circles</title>
-  <link rel="stylesheet" href="../css/styles.css">
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <nav class="navbar">
